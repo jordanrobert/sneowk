5cf1e62ff1f16b63856b5f4e3b7867204c9c0c03
Jordan Robert <jrobert@legalzoom.com>
2020-10-14 18:51:29 -0700

    fix casing

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React, { useReducer } from 'react';
+import Context from './context';
+import Game from './components/game';
+import reducer from './reducer';
+
+const initialState = {
+    snakeCoords: [
+        [1, 1], // tail
+        [2, 1],
+        [3, 1], // head
+    ],
+    foodCoords: [1, 1],
+    paused: true,
+    gameOver: false,
+    direction: 'right',
+    score: 0,
+};
+
+function App() {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    return (
+        <Context.Provider value={[state, dispatch]}>
+            <Game></Game>
+        </Context.Provider>
+    )
+}
+
+export default App;
